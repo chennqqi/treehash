a26da2b887dbd2eedb9be1b7dcfd7fc8fdf97cfa
Chris Downes <christopher.downes@datadoghq.com>
2019-01-25 23:25:04 -0500

    use test table

--- treehash_test.go
+++ treehash_test.go
@@ -5,15 +5,39 @@ import (
 	"fmt"
 	"io"
 	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
 func TestTreeHash(t *testing.T) {
-	// create 1.5MB empty file
-	b := make([]byte, 1536000)
-	th := New()
-	io.Copy(th, bytes.NewReader(b))
-	hash := fmt.Sprintf("%x", th.Sum(nil))
-	if hash != "ca1f50f208ffe93e48045d48359cf31109b852601016c33db554735964d9e636" {
-		t.Fatal("incorrect checksum")
+	tests := []struct {
+		desc     string
+		numBytes uint
+		checksum string
+	}{
+		{
+			desc:     "test for less than 1MB",
+			numBytes: 500000,
+			checksum: "6bb6aefaeaa4e19112e566b467c4301463a30b0a15b9c8248a00ed9cd8e5946b",
+		},
+		{
+			desc:     "test for even number of file chunks",
+			numBytes: 1500000,
+			checksum: "f1cdd9e081e997f7a62522dce66d827deac748852a4a133b29458e02b43fc124",
+		},
+		{
+			desc:     "test for odd number of file chunks",
+			numBytes: 2500000,
+			checksum: "ee1ddc44cb2d853ee2197d3048dfc4fc456dd71d0ad4ec745fab7a50a50ab401",
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.desc, func(t *testing.T) {
+			b := make([]byte, test.numBytes)
+			th := New()
+			io.Copy(th, bytes.NewReader(b))
+			assert.Equal(t, fmt.Sprintf("%x", th.Sum(nil)), test.checksum)
+		})
 	}
 }
